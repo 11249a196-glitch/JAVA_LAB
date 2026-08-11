106e964c6554e929b4df3289c3ab1c7aaacb1940
11249a196-glitch <11249a196@kanchiuniv.ac.in>
2026-08-11 15:06:59 +0530

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+class Dog extends Animal {
+   Dog() {
+   }
+
+   void bark() {
+      System.out.println("Dog is barking");
+   }
+}
