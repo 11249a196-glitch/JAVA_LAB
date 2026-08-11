ee4f42bc825020a7266b02f4fdf2d496b51441c2
11249a196-glitch <11249a196@kanchiuniv.ac.in>
2026-08-11 15:11:07 +0530

    Create Puppy.java

---
+++ Puppy.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+class Puppy extends Dog {
+   Puppy() {
+   }
+
+   void play() {
+      System.out.println("Puppy is playing");
+   }
+}
